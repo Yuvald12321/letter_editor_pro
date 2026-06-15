eaa010ee64f2a6becc80742172c9e921496b5eb6
Yuval <yuvalivshits@gmail.com>
2026-06-15 19:59:26 +0300

    not done

--- main.py
+++ main.py
@@ -37,10 +37,12 @@ class LetterEditorPro(get_code()):
         self.update_button = ctk.CTkButton(self.bottom_bar, text="Update", width=100, command=self.update)
         self.update_button.pack(side="right", padx=5, pady=5)
 
-        self.apply_theme_button = ctk.CTkButton(self.bottom_bar, text="Apply Theme", width=100, command=self.apply_theme)
+        self.apply_theme_button = ctk.CTkButton(self.bottom_bar, text="Apply Theme", width=100, command=self.load_new_theme)
         self.apply_theme_button.pack(side="right", padx=5, pady=5)
 
         self.bottom_bar.grid(row=2, column=0, sticky="ew", padx=10, pady=(0, 10))
+
+        self.setup_tasks()
 
     def update(self):
         if getattr(sys, "frozen", False):
@@ -58,7 +60,7 @@ class LetterEditorPro(get_code()):
         if path.exists():
             ctk.set_default_color_theme(str(path))
 
-    def apply_theme(self):
+    def load_new_theme(self):
         if getattr(sys, "frozen", False):
             path = Path(sys.executable).resolve().parent / "theme.json"
         else:
@@ -70,6 +72,47 @@ class LetterEditorPro(get_code()):
                 theme = json.loads(org.read_text())
                 path.write_text(json.dumps(theme, indent=4))
                 self.destroy()
+
+    def setup_tasks(self):
+        self.tasks_list = []
+
+        self.tasks_window = ctk.CTkToplevel(self)
+        self.tasks_window.title("Tasks")
+        self.tasks_window.transient(self)
+
+        self.tasks_label = ctk.CTkLabel(self.tasks_window, text="Tasks")
+        self.tasks_label.pack(side="top")
+
+        self.tasks_frame = ctk.CTkScrollableFrame(self.tasks_window)
+
+        self.tasks_frame.pack(fill="both", expand=True, padx=5)
+
+        self.add_task_frame = ctk.CTkFrame(self.tasks_window)
+
+        self.add_task_entry = ctk.CTkEntry(self.add_task_frame, placeholder_text="Enter Task")
+        self.add_task_entry.pack(side="left", fill="x", expand=True, padx=5, pady=5)
+        self.add_task_entry.bind("<Return>", lambda _: self.add_task(self.add_task_entry.get()))
+
+        self.add_task_button = ctk.CTkButton(self.add_task_frame, text="Add", width=1, command=lambda: self.add_task(self.add_task_entry.get()))
+        self.add_task_button.pack(side="right", padx=(0, 5), pady=5)
+
+        self.add_task_frame.pack(side="bottom", fill="x", padx=5, pady=5)
+
+    def update_tasks(self):
+        for child in self.tasks_frame.winfo_children():
+            child.destroy()
+        for n, i in enumerate(self.tasks_list):
+            ctk.CTkButton(self.tasks_frame, text="X", width=1, fg_color="transparent", hover_color="red", command=lambda: self.remove_task(i)).grid(row=n, column=1, sticky="e", padx=5, pady=5)
+            ctk.CTkCheckBox(self.tasks_frame, text=i).grid(row=n, column=0, sticky="w", padx=5, pady=5)
+
+    def add_task(self, task):
+        self.tasks_list.append(task)
+        self.add_task_entry.delete(0, "end")
+        self.update_tasks()
+
+    def remove_task(self, task):
+        self.tasks_list.remove(task)
+        self.update_tasks()
 
 
 if __name__ == "__main__":
