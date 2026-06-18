46d34d19ef455a3014e350ab4ad894b0d3260218
Yuval <yuvalivshits@gmail.com>
2026-06-18 12:19:51 +0300

    bat update

--- compile.bat
+++ compile.bat
@@ -1,3 +1,3 @@
-pyinstaller --onefile --noconsole --noconfirm --icon "logo.ico" -n "The letter editor pro" main.py
+call ".venv\Scripts\pyinstaller.exe" --specpath "build" --onefile --noconsole --icon "logo.ico" -n "The letter editor pro" main.py
 rd /S /Q "build"
 "D:\Inno Setup\ISCC.exe" setup.iss
